c7c1842600622ddd5a5e05a8c044e6c0ce12f92b
Igor Soloviev <iasoloviev61@gmail.com>
2017-07-28 12:54:54 +0300

    add social icons and links

--- markup/components/social-icons/data/data.js
+++ markup/components/social-icons/data/data.js
@@ -1,1 +1,17 @@
-var data = {'social-icons': {}};
+socialIcons: {
+    vk: {
+        socialLink: "https://vk.com/id3040974",
+        socialIcon: '#vk',
+        socialName: 'vk'
+    },
+    ln: {
+        socialLink: "https://vk.com/id3040974",
+        socialIcon: '#in',
+        socialName: 'in'
+    },
+    github: {
+        socialLink: "https://vk.com/id3040974",
+        socialIcon: '#github',
+        socialName: 'github'
+    }
+}
